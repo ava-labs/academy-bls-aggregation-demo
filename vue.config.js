f74de29b1917bad1779db2436de4d5cb2a3e7687
Martin Eckardt <martin.eckardt@avalabs.org>
2023-11-22 23:24:30 +0100

    Update title and vue config for pages

--- vue.config.js
+++ vue.config.js
@@ -2,9 +2,13 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
 
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/academy-bls-aggregation-demo'
+    : '/',
+
   pluginOptions: {
     vuetify: {
-			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
-		}
+      // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
+    }
   }
 })
