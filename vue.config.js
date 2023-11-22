3cc0491f0e2b02be4e147254c5df61ba2b87a7cf
Martin Eckardt <martin.eckardt@avalabs.org>
2023-11-22 23:36:32 +0100

    Add slash

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ module.exports = defineConfig({
   transpileDependencies: true,
 
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/academy-bls-aggregation-demo'
+    ? '/academy-bls-aggregation-demo/'
     : '/',
 
   pluginOptions: {
